c72cc59a00bb82da4927896af722d9e70354c4fa
JuniorMaia2394 <maiajr2394@gmail.com>
2020-11-04 18:55:18 -0300

    Estudando Js

---
+++ numerosAlgunsCuidados.js
@@ -0,0 +1,13 @@
+console.log(7/0)
+console.log('3'+ 2) //aqui a string ganha!
+console.log('3' - 2)
+let a = '10'
+let b = ('10'/2)
+console.log(typeof a, b)
+
+console.log("show!" * 2)
+console.log(0.1 + 0.7)
+
+//console.log(10.toString())
+
+console.log((10.345).toFixed(2))
