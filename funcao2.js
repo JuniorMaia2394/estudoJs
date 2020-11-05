548cc55c0d5239e286b64b5c1459672601cdc8af
JuniorMaia2394 <maiajr2394@gmail.com>
2020-11-05 16:11:23 -0300

    null&Undefined, funcoes, var

---
+++ funcao2.js
@@ -0,0 +1,20 @@
+// armazenando uma funçao em uma variavel
+const imprimirSoma = function(a,b){
+    console.log(a+b)
+}
+
+imprimirSoma(2,3)
+
+//armazenando uma funcao arrou em uma variavel
+const soma = (a,b) =>{
+    return a + b
+}
+
+console.log(soma(2,3))
+
+// retorno implicito, arrow funciona como uma função
+const subtracao = (a,b) => a - b
+console.log(subtracao(2,3))
+
+const imprimir2 = a => console.log(a)
+imprimir2('Legal!!')
---
+++ usandoVar1.js
@@ -0,0 +1,21 @@
+{
+    {
+        {
+            { 
+                var sera = 'Será???' //fica visivel de forma global
+                
+            }
+         }
+     }
+}
+
+console.log(sera)
+
+
+function teste(){
+    var local = 123
+    console.log(local) // fica visivel so dentro da função
+}
+
+teste()
+
