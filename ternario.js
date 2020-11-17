b3fa943d28d2413b53dcbf87d341ebd05e33065a
JuniorMaia2394 <maiajr2394@gmail.com>
2020-11-17 15:36:17 -0300

    operadores unarios e ternarios

---
+++ ternario.js
@@ -0,0 +1,6 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+// operador ternario formado por uma variavel, operador de relação 
+// ? o operador ternario e as opçoes no caso 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
---
+++ unarios.js
@@ -0,0 +1,11 @@
+let num1 = 1
+let num2 = 2
+
+num1++
+console.log(num1)
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--)
+//adicionar primeiro (1)+1 =2 === tira depois (2) -1. por isso que dar true 
+console.log(num1 === num2)
